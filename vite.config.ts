67d5822876df287885747298837354c5ee3ce9cc
Khanh Nguyen <155944105+hi-encacap@users.noreply.github.com>
2024-01-23 07:59:28 +0000

    ci: update test coverage file extension

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "json"],
       reportsDirectory: "./test/coverage",
+      extension: [".ts", ".tsx"],
     },
   },
 });
