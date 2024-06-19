47d973108095a3106c24225f17e3352dd8a57a6c
Khanh Nguyen <khanh.nguyen@members.encacap.com>
2024-06-19 02:12:55 +0000

    chore: update vite.config.ts plugins and add checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 /// <reference types="vitest" />
 
-import eslintPlugin from "@nabla/vite-plugin-eslint";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import webfontDownload from "vite-plugin-webfont-dl";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload(), tsconfigPaths(), eslintPlugin()],
+  plugins: [
+    react(),
+    webfontDownload(),
+    tsconfigPaths(),
+    !process.env.VITEST &&
+      checker({
+        typescript: true,
+        eslint: {
+          // for example, lint .ts and .tsx
+          lintCommand: "eslint --report-unused-disable-directives",
+          useFlatConfig: true,
+        },
+      }),
+  ],
   test: {
     environment: "jsdom",
     setupFiles: "./vitest.prepare.ts",
